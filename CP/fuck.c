1f6069e99a1ad35edf9c0abaaed4b9f9d3822e73
CrazyByDefault <es16btech11025@iith.ac.in>
2016-11-04 17:51:31 +0530

    fixed my calendar printing shit, ./fuck

--- CP/fuck.c
+++ CP/fuck.c
@@ -3,27 +3,23 @@
 void main(){
 	printf("\n");
 	int pos;
-	for(pos = 0; pos < 5; pos ++){
-		//printf("\x1B[A\e[9C");
-		int i;
-		//scanf("%d", &pos);
+	
+	int i;
+	scanf("%d", &pos);
+	
+	for(i = 1; i <= 31; i++){		
+		int x = i%7;
+		int space = pos*20 + pos;
+		printf("\x1B[C\e[%dC", space);
+		for(x = i%7; x < 7; x++){
+			if(!(i/10))printf(" ");
+			printf("%d ", i);
+			i++;
+		}printf("\n");
 		
-		for(i = 1; i <= 31; i++){		
-			int x = i%7;
-			int space = pos*20 + pos;
-			printf("\x1B[C\e[%dC", space);
-			for(x = i%7; x < 7; x++){
-				if(!(i/10))printf(" ");
-				printf("%d ", i);
-				i++;
-			}
-			
-			
-			//printf("\x1B[A\e[10C");
-			
-			
-		}
 		
-	}
-	printf("\n");
+		//printf("\x1B[A\e[10C");
+			
+			
+		}printf("\n");
 }
