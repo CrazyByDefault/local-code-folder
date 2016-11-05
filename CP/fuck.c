9087a9a78104656a9dab4e7af16331daef32e051
CrazyByDefault <es16btech11025@iith.ac.in>
2016-11-05 23:19:38 +0530

    Lambda session on js

--- CP/fuck.c
+++ CP/fuck.c
@@ -6,20 +6,28 @@ void main(){
 	
 	int i;
 	scanf("%d", &pos);
+	printf("Hello world\nFuck this shit\nYour mom is a whore\n");
 	
 	for(i = 1; i <= 31; i++){		
 		int x = i%7;
 		int space = pos*20 + pos;
 		printf("\x1B[C\e[%dC", space);
+		int s = 1;
+		//printf("\x1B[A\e[%dC", s);
 		for(x = i%7; x < 7; x++){
+			
+			
 			if(!(i/10))printf(" ");
 			printf("%d ", i);
 			i++;
-		}printf("\n");
+		}
+
+		//printf("\n");
+		//s++;
 		
 		
 		//printf("\x1B[A\e[10C");
 			
 			
-		}printf("\n");
+	}printf("\n");
 }
