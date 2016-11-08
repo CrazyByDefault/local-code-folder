485f87914ce058d3b0ad753bd272a3dc57adb017
CrazyByDefault <es16btech11025@iith.ac.in>
2016-11-08 19:19:38 +0530

    FUCK THE WORLD CALENDAR IS COMPLETE

--- IntroToC/fuckthiscourse/calendar2.c
+++ IntroToC/fuckthiscourse/calendar2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int a[12][6][7]/*, b[6][7], c[6][7]*/;
+int a[12][6][7];
+
+int prevChoice = 0, prevMonth = 0, prevYear = 0;
 
 int md[2][12] = {
  		
@@ -13,53 +15,12 @@ int md[2][12] = {
 
 char *dayNames[7] = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"};
 char *monthNames[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+char *threeMonths[4] = {"January		     February   	     March", "April		       May 		     June", "July		     August	   	   September", "October	  	     November	    	   December"};
 
 //Determine if year is a leap year
 int l=0;
 
-/*int daysSince(int d, int m, int y){
 
-	int days;
-
-	int month, date;
-	
-	//Add all days until last year
-	days = (y-1900)*365 + (y-1900)/4 - (y-1900)/100 + (y-1900)/400;
-
-	//Check if the year given is a leap year or not
-	
-
-	//Run a loop for that year, till we reach the given day
-	for(month = 1; month <=m; month++){
-		
-
-		if(month == m){
-			//If the current month is reached, 
-			days = days + d;
-			
-			break;
-		}
-		else{
-
-			//Run a loop till the last day of the month, as specified in md[][]
-			for(date = 1; date <= md[l][month-1]; date++){
-				days++;
-			}
-		}
-	}
-
-	/*Check which day of the week it is
-	int x = days%7;
-
-	printf("%s \n", names[x]);
-
-	return days;
-
-}
-
-int newDaysSince(int d, int m, int y){
-
-}*/
 
 int dayOfWeek(int d, int m, int y){
 	int days;
@@ -99,8 +60,6 @@ int dayOfWeek(int d, int m, int y){
 	return x;
 
 	//printf("%s \n", names[x]);
-
-
 }
 
 void zeroArray(){
@@ -116,10 +75,33 @@ void zeroArray(){
 	}
 }
 
-void printMonth(int m, int y){
+int printMonth(){
+	int m, y;
+
+	if(prevChoice != 2){
+		printf("Enter the month\n");
+		scanf("%d", &m);
+		printf("Enter the year\n");
+		scanf("%d", &y);
+		printf("\n");
+		prevMonth = m;
+		prevYear = y;
+	}else{
+		m = prevMonth;
+		y = prevYear;
+	}
+
+	if(y%4 == 0)l = 1;
+	if(y%100 ==0 && y%400 != 0)l = 0;
+
+	if(m < 0 || m > 12 || y < 1900 || y > 2016){
+		printf("Please enter a valid date.\n");
+		return -1;
+	}
+
 	int i, j, c = dayOfWeek(1, m, y), r = 0;
 	
-
+	printf("\t%s\n", monthNames[m-1]);
 	for(i = 0; i < 7; i++)printf("%s ", dayNames[i]);
 	printf("\n");
 
@@ -131,6 +113,7 @@ void printMonth(int m, int y){
 		if(c%7 == 0)r++;
 		
 	}
+
 
 	for (i = 0; i < 6; i++){
 		for(j = 0; j < 7; j++){
@@ -144,10 +127,30 @@ void printMonth(int m, int y){
 
 		
 	}
-
+	zeroArray();
+	prevChoice = 2;
+	return 0;
 }
 
-void printYear(int y){
+int printYear(){
+	int y;
+
+	if(prevChoice != 1){
+		printf("Enter the year\n");
+		scanf("%d", &y);
+		printf("\n");
+		prevYear = y;
+	}else y = prevYear;
+
+	if(y%4 == 0)l = 1;
+	if(y%100 ==0 && y%400 != 0)l = 0;
+
+
+	if(y < 1900 || y > 2016){
+		printf("Please enter a valid date.\n");
+		return -1;
+	}
+
 	int i, j, k;
 
 	for(k = 0; k < 12; k++){
@@ -163,13 +166,19 @@ void printYear(int y){
 		
 		}
 	}
-
-	for(k = 0; k < 12; k++){
+	int balls = 0;
+	for(k = 0; k < 10; k += 3){
 		// printf("K = %d\n", k);
-		printf("\t%s\n", monthNames[k]);
-		for(i = 0; i < 7; i++)printf("%s ", dayNames[i]);
+		printf("\t\t\t      %d\n", y);
+		printf("\t%s\n", threeMonths[balls]);
+		balls++;
+		for(j = 0; j < 3; j++){
+			for(i = 0; i < 7; i++)printf("%s ", dayNames[i]);
+			printf("  ");
+		}
 		printf("\n");
 		for (i = 0; i < 6; i++){
+
 			for(j = 0; j < 7; j++){
 
 				if(a[k][i][j] == 0)printf("   ");
@@ -177,31 +186,112 @@ void printYear(int y){
 				else printf("%d ", a[k][i][j]);
 
 
-			}printf("\n");
+			} //Switch to next month, same line here
+			printf("  ");
+
+			for(j = 0; j < 7; j++){
+
+				if(a[k+1][i][j] == 0)printf("   ");
+				else if(a[k+1][i][j]/10 == 0)printf("%d  ", a[k+1][i][j]); 
+				else printf("%d ", a[k+1][i][j]);
+
+
+			}
+			printf("  ");
+
+			for(j = 0; j < 7; j++){
+
+				if(a[k+2][i][j] == 0)printf("   ");
+				else if(a[k+2][i][j]/10 == 0)printf("%d  ", a[k+2][i][j]); 
+				else printf("%d ", a[k+2][i][j]);
+
+
+			}
+			printf("\n");
 
 			
 		}printf("\n\n");
 	}
-
+	zeroArray();
+	prevChoice = 1;
+	return 0;
 }
+
+
+int moveRight(){
+	if(prevChoice == 0 || prevYear == 0)return -1;
+	
+	if(prevChoice == 1){
+		prevYear++;
+		printYear();
+		return 0;
+	}
+
+	if(prevChoice == 2){
+		if(prevMonth == 0)return -1;
+		if(prevMonth != 12)prevMonth++;
+		else{
+			prevMonth = 1;
+			prevYear++;
+		}
+		printMonth();
+		return 0;
+	}
+}
+
+int moveLeft(){
+	if(prevChoice == 0 || prevYear == 0)return -1;
+	
+	if(prevChoice == 1){
+		prevYear--;
+		printYear();
+		return 0;
+	}
+
+	if(prevChoice == 2){
+		if(prevMonth == 0)return -1;
+		if(prevMonth != 1)prevMonth--;
+		else{
+			prevMonth = 12;
+			prevYear--;
+		}
+		printMonth();
+		return 0;
+	}
+}
+
+int calendarMenu(){
+	printf("Calendar Menu\n1:Display Year\n2:Display Month\n3:Move right\n4:Move left\n5:Exit\n");
+	int choice;
+	scanf("%d", &choice);
+
+	switch(choice){
+		
+		case 1:
+			if(!printYear())calendarMenu();
+			return 0;
+
+		case 2:
+			if(!printMonth())calendarMenu();
+			return 0;
+
+		case 3:
+			if(!moveRight())calendarMenu();
+			return 0;
+
+		case 4:
+			if(!moveLeft())calendarMenu();
+			return 0;
+
+		case 5:
+			return 0;
+	}
+}
+
+
 
 int main(){
 	zeroArray();
-	int m, y;
-	//scanf("%d", &d);
-	//printf("\n");
-	// scanf("%d", &m);
-	// printf("\n");
-	scanf("%d", &y);
-	printf("\n");
+	return calendarMenu();
 
-	if(y%4 == 0)l = 1;
-	if(y%100 ==0 && y%400 != 0)l = 0;
-
-	if(/*d < 0 || d > md[l][m - 1] || m < 0 || m > 12 ||*/ y < 1900 || y > 2016){
-		printf("Please enter a valid date.\n");
-		return -1;
-	}
-
-	printYear(y);
 }
