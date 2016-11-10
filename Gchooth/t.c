07b1c7e79c685c54e105ab4c2c69684acf0c9524
CrazyByDefault <es16btech11025@iith.ac.in>
2016-11-10 17:18:35 +0530

    tp

--- Gchooth/t.c
+++ Gchooth/t.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int a[3];
 int month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+char *Monthname[]={"January","Febraury","March","April","May","June","July","August","September","October","November","December"};
+char *threeMonths[4] = {"January		Febraury		March", "April		May		June", "July		August		September", "October		November		December"};
+char *weekday[]={"Su","Mo","Tu","We","Th","Fr","Sa"};
+int pChoice = 0, pYear = 0, pMonth = 0;
 
 int fday(int x, int y){
 	int s=0,i;
@@ -13,13 +17,138 @@ int fday(int x, int y){
 	s=((y-1)*365+(y-1)/4-(y-1)/100+(y-1)/400+s+1)%7;
 return s;
 }
-int main(){
+
+int moveRight(){
+	if(pChoice == 0 || pYear == 0)return -1;
+	
+	if(pChoice == 1){
+		pYear++;
+		printYear();
+		return 0;
+	}
+
+	if(pChoice == 2){
+		if(pMonth == 0)return -1;
+		if(pMonth != 12)pMonth++;
+		else{
+			pMonth = 1;
+			pYear++;
+		}
+		printMonth();
+		return 0;
+	}
+}
+
+
+//Move left, similar to moveRight()
+int moveLeft(){
+	if(pChoice == 0 || pYear == 0)return -1;
+	
+	if(pChoice == 1){
+		pYear--;
+		printYear();
+		return 0;
+	}
+
+	if(pChoice == 2){
+		if(pMonth == 0)return -1;
+		if(pMonth != 1)pMonth--;
+		else{
+			pMonth = 12;
+			pYear--;
+		}
+		printMonth();
+		return 0;
+	}
+}
+
+int calendarMenu(){
+	printf("Calendar Menu\n1:Display Year\n2:Display Month\n3:Move right\n4:Move left\n5:Exit\n");
+	int choice;
+	scanf("%d", &choice);
+
+	switch(choice){
+		
+		case 1:
+			if(!printYear())calendarMenu();
+			return 0;
+
+		case 2:
+			if(!printMonth())calendarMenu();
+			return 0;
+
+		case 3:
+			if(!moveRight())calendarMenu();
+			return 0;
+
+		case 4:
+			if(!moveLeft())calendarMenu();
+			return 0;
+
+		case 5:
+			return 0;
+	}
+}
+
+int printMonth(){
+	int i, j, k, m, y, blank;
+	if(pChoice!=2){
+		printf("Enter a month:\n");
+		scanf("%d", &m);
+		printf("Enter year:\n");
+		scanf("%d", &y);
+	}else {
+		m = pMonth;
+		y = pYear;
+	}
+
+	printf("\t%s\n", Monthname[m-1]);
+
+	//printf("\n");	
+	//printf("\t%s\n", threeMonths[p/4]);	
+    
+
+	for(int i=0;i<7;i++)printf("%s ",weekday[i]);
+	int c=0;
+    
+    printf("\n");
+ 	
+ 		
+	blank=fday(m,y);
+	for(i=0;i<blank;i++){
+		printf("   ");
+		c=c+1;
+
+ 	}
+ 	for(i = 1; i <= (7-c); i++){
+ 		printf("%d  ", i);
+ 	}printf("\n");
+
+
+ 	
+	for(j = 8 - c; j < month[m-1]; j++){
+		if(j/10 == 0)printf(" ");
+		printf("%d ", j);
+		if ((j - 7 + c)%7 == 0)printf("\n");
+	}printf("\n");
+
+ 	pChoice = 2;
+ 	pMonth = m;
+ 	pYear = y;
+	return 0;
+
+
+}
+
+int printYear(){
 	int i, y, j, k , p, blank,m,c,d;
-	printf("enter year\n");
-	scanf("%d",&y);
-	char *Monthname[]={"January","Febraury","March","April","May","June","July","August","September","October","November","December"};
-	char *threeMonths[4] = {"January		Febraury		March", "April		May		June", "July		August		September", "October		November		December"};
-	char *weekday[]={"Su","Mo","Tu","We","Th","Fr","Sa"};
+	if(pChoice!=1){
+		printf("Enter an year\n");
+		scanf("%d",&y);
+	}else{
+		y = pYear;
+
+	}
 	
 	
 
@@ -30,7 +159,7 @@ int main(){
 			printf("\t%s\n", threeMonths[p/4]);	
 		    for(m=1;m<=3;m++){
 
-		    	for(int i=0;i<7;i++)printf("%s ",weekday[i]);
+				for(int i=0;i<7;i++)printf("%s ",weekday[i]);
 		    	printf("\t\t");
 		    }
 		 	printf("\n");
@@ -64,6 +193,15 @@ int main(){
 	 		}
 
 	 	printf("\n");
+	 	pChoice = 1;
+	 	pYear = y;
+	 	return 0;
+}
+
+
+int main(){
+	calendarMenu();
+	
 	    
 	return 0;
 }
